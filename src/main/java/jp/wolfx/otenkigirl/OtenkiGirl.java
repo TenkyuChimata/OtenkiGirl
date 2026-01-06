d7dae82a1854809031c384e96a847b8ca07ed263
TenkyuChimata <tenkyuchimata@gmail.com>
2026-01-06 22:39:53 +0800

    Fix Folia

--- src/main/java/jp/wolfx/otenkigirl/OtenkiGirl.java
+++ src/main/java/jp/wolfx/otenkigirl/OtenkiGirl.java
@@ -2,37 +2,33 @@ package jp.wolfx.otenkigirl;
 
 import java.util.Objects;
 
-import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffectType;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public final class OtenkiGirl extends JavaPlugin {
-    private static Economy econ = null;
-    private static double price;
-    private static String pMsg;
-    private static String bMsg;
-    private static String moneyMsg;
-    private static String noStorm;
+    private Economy econ = null;
+    private double price;
+    private String pMsg;
+    private String bMsg;
+    private String moneyMsg;
+    private String noStorm;
+    private final boolean folia = isFolia();
 
-    private static boolean isFolia() {
+    private boolean isFolia() {
         try {
             Class.forName("io.papermc.paper.threadedregions.scheduler.AsyncScheduler");
             return true;
         } catch (ClassNotFoundException e) {
             return false;
         }
-    }
-
-    public static ScheduledTask scheduleSyncDelayedTask(Plugin plugin, Runnable task, long delay){
-        return Bukkit.getGlobalRegionScheduler().runDelayed(plugin, (t) -> task.run(), delay != 0 ? delay : 1 );
     }
 
     @Override
@@ -62,27 +58,28 @@ public final class OtenkiGirl extends JavaPlugin {
         } else if (!econ.withdrawPlayer(player, price).transactionSuccess()) {
             player.sendMessage(String.format(moneyMsg, price));
         } else {
-            if (!isFolia()) {
+            if (!folia) {
                 player.getWorld().setStorm(false);
             } else {
-                Bukkit.getGlobalRegionScheduler().run(this, task -> player.getWorld().setStorm(false));
+                Bukkit.getGlobalRegionScheduler().run(this, _ -> player.getWorld().setStorm(false));
             }
-            this.getServer().broadcastMessage(String.format(bMsg, player.getDisplayName()));
+            this.getServer().broadcast(LegacyComponentSerializer.legacySection().deserialize(String.format(bMsg, player.getName())));
             player.sendMessage(pMsg);
-            if (!isFolia()) {
+            if (!folia) {
                 this.getServer().getScheduler().runTaskLater(this, () -> {
                     if (player.isValid()) {
                         player.setHealth(player.getHealth() * 0.5);
-                        player.addPotionEffect(PotionEffectType.SLOW.createEffect(2400, 0));
-                        player.addPotionEffect(PotionEffectType.LEVITATION.createEffect(100, 0));
+                        player.addPotionEffect(PotionEffectType.SLOWNESS.createEffect(120 * 20, 2));
+                        player.addPotionEffect(PotionEffectType.LEVITATION.createEffect(10 * 20, 0));
                     }
                 }, 100L);
             } else {
-                scheduleSyncDelayedTask(this, () -> {
-                    if (player.isValid()) {
-                        player.setHealth(player.getHealth() * 0.5);
-                    }
-                }, 100L);
+                player.getScheduler().runDelayed(this, _ -> {
+                    if (!player.isValid()) return;
+                    player.setHealth(player.getHealth() * 0.5);
+                    player.addPotionEffect(PotionEffectType.SLOWNESS.createEffect(120 * 20, 2));
+                    player.addPotionEffect(PotionEffectType.LEVITATION.createEffect(10 * 20, 0));
+                }, null, 100L);
             }
         }
     }
